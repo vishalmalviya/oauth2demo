13e5c558c5fe808a28a541480c7e793184c69982
vishalmalviya <visu.malviya@gmail.com>
2024-12-27 17:05:17 +0530

    Initial commit

---
+++ demo/src/main/java/oauth2/demo/OAuth2DemoApplication.java
@@ -0,0 +1,13 @@
+package oauth2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/main/java/oauth2/demo/SecurityConfig.java
@@ -0,0 +1,21 @@
+package oauth2.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests
+                        (auth -> auth.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
